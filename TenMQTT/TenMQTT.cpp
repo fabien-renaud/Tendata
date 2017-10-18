0f0b40d576bf29ccbfc3bb9dea3e63488aacec2c
Fabien RENAUD <frd.frenaud@gmail.com>
2017-10-18 20:08:41 +0200

    maj TenMQTT.cpp
    maj TenMQTT.cpp (code sample remplacé)

--- TenMQTT/TenMQTT.cpp
+++ TenMQTT/TenMQTT.cpp
@@ -1,14 +1,15 @@
 #include "TenMQTT.h"
+#include <mqtt\async_client.h>
 
-TenMQTT::TenMQTT()
-{
+TenMQTT::TenMQTT() {
+
 }
 
+TenMQTT::~TenMQTT() {
 
-TenMQTT::~TenMQTT()
-{
 }
 
 int main() {
+	mqtt::async_client client();
 	return 0;
 }
